c0fe295fb2d1c083710a13bd1d35ee5b0c9b3c9d
Marina Shirmanova <laputa94plus@gmail.com>
2023-04-02 12:11:51 +0300

    add new class GetAgeDiffTask

---
+++ src/GetAgeDiffTask.java
@@ -0,0 +1,13 @@
+import java.math.*;
+
+public class GetAgeDiffTask {
+
+    /*
+    Реализовать метод, который будет принимать два числа, выражающие возраст людей, и возвращать разницу в возрасте.
+     */
+
+    public static byte getAgeDiff(byte age1, byte age2) {
+        int dif = age1 - age2;
+        return (byte) Math.abs(dif);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // System.out.println(СharExpression.charExpression(5));
+        // System.out.println(GetAgeDiffTask.getAgeDiff((byte) 4, (byte) 1));
     }
 }
